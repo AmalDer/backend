88c3adae424857a7111bf561a37cfeb5b95e7840
chaimae2000 <80167259+chaimae2000@users.noreply.github.com>
2022-07-01 00:56:36 +0100

    last commit

--- routes/student.js
+++ routes/student.js
@@ -4,6 +4,12 @@ var router = express.Router();
 
 var database = require('../database');
 
+const bodyParser = require('body-parser')
+router.use( bodyParser.json() );       // to support JSON-encoded bodies
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 router.get("/", function(request, response, next){
 
 	var query = "SELECT * FROM student ORDER BY idstudent ASC";
@@ -31,12 +37,12 @@ router.get("/add", function(request, response, next){
 });
 
 router.post("/add_student", function(request, response, next){
-
-	var idstudent = request.body.idstudent;
+	console.log(request.body)
+ 	 var idstudent = request.body.idstudent;
 
 	var iduser = request.body.iduser;
 
-	var idgroup = request.body.idgroup;
+	var idgroup = request.body.idgroup; 
 
 	var niveau = request.body.niveau;
 
@@ -48,13 +54,18 @@ router.post("/add_student", function(request, response, next){
 
 	var genie = request.body.genie;
 
-	var query = `
-	INSERT INTO student 
-	(idstudent,iduser,idgroup,niveau,nom,prenom,email,genie) 
-	VALUES ("${idstudent}", "${iduser}", "${idgroup}","${niveau}","${nom}","${prenom}","${email}","${genie}")
-	`;
+	var query = `INSERT INTO student (idstudent,iduser,idgroup,niveau,nom,prenom,email,genie) VALUES ("${idstudent}","${iduser}","${idgroup}","${niveau}","${nom}","${prenom}","${email}","${genie}")`;
+ 
 
-	database.query(query, function(error, data){
+	/* const studentDetails = request.body;
+	var sql = 'INSERT INTO student SET  ?';
+	database.query(sql, studentDetails, function (err,data) {
+		if(err) throw err;
+		console.log("success");
+		
+	});
+	response.redirect("/student"); */
+	 database.query(query, function(error, data){
 
 		if(error)
 		{
@@ -65,7 +76,7 @@ router.post("/add_student", function(request, response, next){
 			response.redirect("/student");
 		}
 
-	});
+	}); 
 
 });
 
